1e78a6565ac15a7c724ec84cf0863e78ce8e193b
Yann Voté <ygversil@lilo.org>
2021-10-28 16:19:26 +0200

    Read download repository from config

--- config_tools.py
+++ config_tools.py
@@ -30,6 +30,7 @@ __copyright__ = '(C) 2021 by Yann Voté'
 __revision__ = '$Format:%H$'
 
 
+from collections import namedtuple
 import pathlib
 
 from qgis.core import (
@@ -37,6 +38,7 @@ from qgis.core import (
     QgsExpressionContextUtils,
 )
 
+from .context_managers import qgis_group_settings
 from .path_tools import is_absolute_folder_path
 
 
@@ -45,9 +47,27 @@ qgis_variable = QgsExpressionContextScope.variable
 global_scope = QgsExpressionContextUtils.globalScope
 
 
+SETTINGS_GROUP = 'Plugin-DownloadSharedFiles/repositories'
+
+
+RepoSettings = namedtuple('Settings', (
+    'url',
+    'files_version',
+))
+
+
 def read_gis_data_home():
     """Read global ``gis_data_home`` QGIS variable and return a
     ``pathlib.Path`` object with it, or ``None`` if it is not valid."""
     path = qgis_variable(global_scope(), 'gis_data_home')
     path = pathlib.Path(path) if path else None
     return path if is_absolute_folder_path(path) else None
+
+
+def read_repository_settings(repo_title):
+    """Read given web repository settings from config file."""
+    with qgis_group_settings(SETTINGS_GROUP) as s:
+        d = dict()
+        d['url'] = s.value('{}/url'.format(repo_title))
+        d['files_version'] = s.value('{}/files_version'.format(repo_title), 0)
+        return RepoSettings(**d)
