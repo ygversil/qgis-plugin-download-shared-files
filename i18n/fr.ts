69310c88fc2c257381637409df2043c32a641fd3
Yann Voté <ygversil@lilo.org>
2021-10-28 14:10:02 +0200

    [i18n] Update

--- i18n/fr.ts
+++ i18n/fr.ts
@@ -12,14 +12,110 @@
 <context>
     <name>DownloadSharedFilesAlgorithm</name>
     <message>
-        <location filename="../download_shared_files_algorithm.py" line="71"/>
-        <source>Input layer</source>
-        <translation type="unfinished"></translation>
+        <location filename="../download_shared_files_algorithm.py" line="89"/>
+        <source>Download shared files</source>
+        <translation>Télécharger fichiers partagés</translation>
     </message>
     <message>
-        <location filename="../download_shared_files_algorithm.py" line="82"/>
-        <source>Output layer</source>
-        <translation type="unfinished"></translation>
+        <location filename="../download_shared_files_algorithm.py" line="97"/>
+        <source>File tools</source>
+        <translation>Outils fichiers</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="242"/>
+        <source>Download report</source>
+        <translation>Rapport de téléchargement</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="118"/>
+        <source>HTML files (*.html)</source>
+        <translation>Fichiers HTML (*.html)</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="131"/>
+        <source>gis_data_home not found. You have to set gis_data_home global variable to the path of your main GIS folder (in Preferences -&gt; Settings dialog, Variables tab).</source>
+        <translation>La variable gis_data_home n'a pas été trouvée. Vous devez définir cette variable en indiquant le chemin vers votre dossier SIG principal (dans le menu Préférences -&amp;gt; Options, onglet Variables).</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="146"/>
+        <source>Aborting on user request</source>
+        <translation>Annulation à la demande de l'utilisateur</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="161"/>
+        <source>Downloading file {}...</source>
+        <translation>Téléchargement du fichier {}...</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="177"/>
+        <source>Unable to download file {fname}</source>
+        <translation>Impossible de télécharger le fichier {fname}</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="182"/>
+        <source>Download complete for file {}. Now checking file integrity</source>
+        <translation>Téléchargement du fichier {} terminé. Vérification de l'intégrité du fichier</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="187"/>
+        <source>SHA1 checksum of downloaded file does not match expected one. Something went wrong during download of file {}. Please try again.</source>
+        <translation>La clé de contrôle SHA1 du fichier téléchargé ne correspond pas à celle attendue. Le téléchargement du fichier {} ne s'est pas déroule correctement. Merci d'essayer à nouveau.</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="194"/>
+        <source>File {} sucessfully downloaded.</source>
+        <translation>Fichier {} téléchargé avec succès.</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="197"/>
+        <source>File {i} of {count} successfully downloaded.</source>
+        <translation>Fichier {i} sur {count} téléchargé avec succès.</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="217"/>
+        <source>&lt;p&gt;All files are up-to-date. No files downloaded&lt;/p&gt;</source>
+        <translation>&amp;lt;p&amp;gt;Tous les fichiers sont à jour. Aucun fichier téléchargé.&amp;lt;/p&amp;gt;</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="220"/>
+        <source>&lt;p&gt;{fname} (version: {version})&lt;/p&gt;
+&lt;p&gt;&lt;strong&gt;Release notes&lt;/strong&gt;. {new}&lt;/p&gt;</source>
+        <translation>&lt;p&gt;{fname} (version : {version})&lt;/p&gt;
+&lt;p&gt;&lt;strong&gt;Notes de version&lt;/strong&gt;. {new}&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="242"/>
+        <source>Downloaded files</source>
+        <translation>Fichiers téléchargés</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="272"/>
+        <source>Unable to read version file for {}. Download it again.</source>
+        <translation>Impossible de lire la version du fichier {}. Nouveau téléchargement.</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="277"/>
+        <source>File {} up-to-date. Nothing to do.</source>
+        <translation>Fichier {} à jour. Rien à faire.</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="297"/>
+        <source>dest_folder empty or not set for file {}.</source>
+        <translation>Paramètre dest_folder vide ou non défini pour le fichier {}.</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="303"/>
+        <source>dest_folder cannot be an absolute path for {}.</source>
+        <translation>Le paramètre dest_folder ne peut pas être un chemin absolu pour {}.</translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="101"/>
+        <source>This algorithm downloads GIS shared files from given web repository. Only files that have been added or updated on the repository are downloaded. Existing files will replaced by the new version on local hard drive.
+
+After successful download, an HTML report is generated showing what files have been downloaded.</source>
+	<translation>Cet algorithme télécharge les fichiers GIS partagés sur le dépôt sélectionné. Seuls les fichiers qui ont été ajouté ou mis à jour sur le dépôt sont téléchargés. Les fichiers existants seront remplacés par les nouvelles versions sur le disque dur local.
+
+Après les téléchargements réussis, un rapport HTML est généré qui montre les fichiers téléchargés.</translation>
     </message>
 </context>
 <context>
@@ -27,7 +123,7 @@
     <message>
         <location filename="../download_shared_files_provider.py" line="77"/>
         <source>Download Shared Files</source>
-        <translation type="unfinished"></translation>
+        <translation>Télécharger fichiers partagés</translation>
     </message>
 </context>
 </TS>
