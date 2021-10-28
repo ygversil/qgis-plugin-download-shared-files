764db76d108b9d47da3f92d2ec57463ed10d4036
Yann Voté <ygversil@lilo.org>
2021-10-28 12:16:01 +0200

    Switch to fr locale

---
+++ i18n/fr.ts
@@ -0,0 +1,33 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr" sourcelanguage="en">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../test/test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation>Bonjour</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadSharedFilesAlgorithm</name>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="70"/>
+        <source>Input layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../download_shared_files_algorithm.py" line="81"/>
+        <source>Output layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DownloadSharedFilesProvider</name>
+    <message>
+        <location filename="../download_shared_files_provider.py" line="75"/>
+        <source>Download Shared Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
